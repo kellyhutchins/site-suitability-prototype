e3d69793efb4dd7de89a395fa5806db2bced4e2d
Nick <nsenger@esri.com>
2017-07-19 17:21:23 -0700

    feat: Implements basic window management

--- src/ts/components/SiteSuitability.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Map } from 'react-arcgis';
-
-export default (props) => (
-    <Map
-        style={{ display: 'inline-block', height: `${props.mapHeight}px`, width: '50%' }}
-        mapProperties={{ basemap: 'osm' as __esri.BasemapProperties }}
-    />
-);
